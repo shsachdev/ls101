4849de71fbde4be1e740b64c7bf715c3c17c9faf
Shikhar Sachdev <shsachdev@gmail.com>
2019-06-18 23:37:52 +0200

    did more examples

--- lesson_4/array_ex.rb
+++ lesson_4/array_ex.rb
@@ -8,7 +8,9 @@ new_array = [1, 2, 3, 4, 5]
 counter = 0
 
 loop do
-  arr[counter] += 1
+  new_array[counter] += 1
   counter += 1
-  break if counter == arr.size
+  break if counter == new_array.size
 end
+
+puts new_array
