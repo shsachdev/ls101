4f8406278a032b7b2453da9b5bd8d0f91bd08a69
Shikhar Sachdev <shsachdev@gmail.com>
2019-06-18 23:21:55 +0200

    loop stuff

---
+++ lesson_4/array_ex.rb
@@ -0,0 +1,14 @@
+# increase value of each number in array by 1
+
+array = [88,9,876,1,2,3]
+
+array.each { |x| puts (x + 1)   }
+
+new_array = [1, 2, 3, 4, 5]
+counter = 0
+
+loop do
+  arr[counter] += 1
+  counter += 1
+  break if counter == arr.size
+end
