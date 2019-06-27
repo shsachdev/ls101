907efa9673358f9f08589af89b60a8142f09a02f
Shikhar Sachdev <shsachdev@gmail.com>
2019-06-28 01:55:56 +0200

    working on q19

---
+++ lesson_4/quiz/q19.rb
@@ -0,0 +1,14 @@
+countries_and_capitals = {
+  'France' => 'Paris',
+  'Belgium' => 'Brussels',
+  'Morocco' => 'Rabat',
+  'Barbados' => 'Bridgetown',
+  'Peru' => 'Lima',
+  'Bolivia' => 'La Paz',
+  'Brazil' => 'Brasilia'
+}
+
+# Given the following hash which contains keys representing countries and
+# values representing the capital cities of those countries, return a new hash
+# containing only the key-value pairs where the country begins
+# with the letter 'B'.
