4c9f4b1bd6c0724e470eb5c17419a6a2d56bc785
Shikhar Sachdev <shsachdev@gmail.com>
2019-06-29 18:37:48 +0200

    each with index

--- lesson_5/exercises/ex14.rb
+++ lesson_5/exercises/ex14.rb
@@ -1,2 +1,13 @@
 contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
 contacts = {"Joe Smith" => {}}
+array = [:email, :address,:number ]
+
+# iterate over the contacts hash and populate the associated data from the contact_data array.
+
+contacts.each do |k,v|
+  array.each do |sym|
+    contacts[k][sym] = contact_data.shift
+  end
+end
+
+puts contacts
