ce8662ffedfa73af9f552dcf8b63f81fdda35cdf
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-01 18:28:03 +0200

    sort q

--- lesson_5/prac_problems/q9.rb
+++ lesson_5/prac_problems/q9.rb
@@ -1,1 +1,10 @@
 arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
+
+# given this data structure, return a new array of the same structure
+# but within the sub arrays being ordered in descending order.
+
+arr.map do |subarray|
+  subarray.sort do |a, b|
+    b <=> a
+  end
+end
