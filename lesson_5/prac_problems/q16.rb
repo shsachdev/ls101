20c88fb1081e05d5e70d210ad65aeca7be21c50d
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-02 19:49:41 +0200

    need to finish q16

---
+++ lesson_5/prac_problems/q16.rb
@@ -0,0 +1,11 @@
+def unique_identifier
+  first_section = rand()
+end
+
+
+
+
+# Each UUID consists of 32 hexadecimal characters, and is typically broken into 5 sections
+# like this 8-4-4-4-12 and represented as a string.
+#
+# It looks like this: "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
