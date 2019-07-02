67de640a012c448ecd4fe5ce6f95d97078ec2d10
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-02 16:25:32 +0200

    f yeah

--- lesson_5/prac_problems/q14.rb
+++ lesson_5/prac_problems/q14.rb
@@ -5,3 +5,31 @@ hsh = {
   'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
   'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
 }
+
+# Given this data structure write some code to return an array containing the colors of the fruits
+# and the sizes of the vegetables. The sizes should be uppercase and the colors
+# should be capitalized.
+
+array = []
+
+hsh.each do |key, value|
+  if value[:type] == "fruit"
+    new_value = value[:colors].map do |elem|
+      elem.capitalize
+    end
+    array << new_value
+  elsif value[:type] == "vegetable"
+    array << value[:size].upcase
+  end
+end
+
+p array
+
+# this code works fine except not upper casing the colors.
+# hsh.each do |key, value|
+#   if value[:type] == "fruit"
+#     array << value[:colors]
+#   elsif value[:type] == "vegetable"
+#     array << value[:size].upcase
+#   end
+# end
