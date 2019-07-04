a4a9b3c34b964c058d0a8ddf61d2cec8d10b22f1
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-04 20:49:21 +0200

    sdcsdc

---
+++ lesson_6/tictactoe.rb
@@ -0,0 +1,15 @@
+# # Tic Tac Toe Flowchart
+#
+# 1. Display the initial empty 3 x 3 board.
+# 2. Ask the user to mark a square.
+# 3. Computer marks a square.
+# 4. Display the updated board state.
+# 5. If winner, display winner.
+# 6. If board is full, display tie.
+# 7. If neither winner nor board is full, go to #2
+# 8. Play again?
+# 9. If yes, go to #1
+# 10. Good bye!
+
+puts ""
+puts ""
