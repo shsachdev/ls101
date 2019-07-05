c7c84056ad7860b5e003ca737ca5375e14fc0ec9
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-05 15:01:23 +0200

    finished tic_tac_toe game

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -20,6 +20,8 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system "clear"
+  puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}."
   puts ""
   puts "     |     |     "
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
@@ -58,9 +60,56 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-board = initialize_board
-display_board(board)
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
 
-player_places_piece!(board)
-puts board.inspect
-display_board(board)
+def board_full?(brd)
+  empty_squares(brd) == []
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] + # rows
+                  [[1,4,7], [2,5,8], [3,6,9]] + # columns
+                  [[1,5,9], [3,5,7]] # diagonals
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER && brd[line[1]] == PLAYER_MARKER && brd[line[2]] == PLAYER_MARKER
+      return "Player"
+    elsif brd[line[0]] == COMPUTER_MARKER && brd[line[1]] == COMPUTER_MARKER && brd[line[2]] == COMPUTER_MARKER
+      return "Computer"
+    end
+  end
+  nil
+end
+
+loop do
+
+  board = initialize_board
+
+  loop do
+    display_board(board)
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+  prompt "Do you want to play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?("y")
+end
+
+prompt "Thanks for playing Tic Tac Toe. Goodbye!"
