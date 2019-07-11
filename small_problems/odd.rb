133f642d820388aae831603d11cb07c54c448d98
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-11 22:46:01 +0200

    odd problem done

---
+++ small_problems/odd.rb
@@ -0,0 +1,15 @@
+def is_odd?(num)
+  if num.abs%2 != 0
+    true
+  else
+    false
+  end
+end
+
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
