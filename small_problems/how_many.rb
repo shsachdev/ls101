66d98049e533eac8c1e38804df6b1e50ebb73bba
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-11 23:32:44 +0200

    tricky how_many q solved

---
+++ small_problems/how_many.rb
@@ -0,0 +1,32 @@
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+
+def count_occurrences(array)
+  hash = {}
+  array.each do |string|
+    if hash.keys.include?(string)
+      hash[string] += 1
+    else
+      hash[string] = 1
+    end
+  end
+  hash.each do |key, value|
+    puts "#{key} => #{value}"
+  end
+end
+
+# array full of strings, some of which occur more than once
+# iterate through array, adding each string to a new hash as keys; values are set to 1 unless the value is already a part
+# of the hash, in which case, simply add 1 to the value.
+# once you have finished iterating through the array, print out all elements of the hash.
+
+
+count_occurrences(vehicles)
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
