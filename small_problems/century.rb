c91860e9ea0834b1563e5820f9eb4fa75ba80442
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-18 14:10:20 +0200

    century_suffix method complete

--- small_problems/century.rb
+++ small_problems/century.rb
@@ -5,24 +5,30 @@ def century(year)
   else
     value = ((year / 100) + 1)
   end
-  century_suffix(value)
+  value.to_s + century_suffix(value)
 end
 
-def century_suffix(year)
-  if value%10 == 1 && value.to_s.include?("11") != true
-    return value.to_s + "st"
-  elsif value
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
+  end
 end
 
-century(2000) == '20th'
-century(2001) == '21st'
-century(1965) == '20th'
-century(256) == '3rd'
-century(5) == '1st'
-century(10103) == '102nd'
-century(1052) == '11th'
-century(1127) == '12th'
-century(11201) == '113th'
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
 
 
 # st => 1, 21, 31, 41, 51, 61, 71, 81, 91, 101,
