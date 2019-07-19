567c9da462342aaf9ae42e5478ce42c9aac816fb
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-19 19:01:46 +0200

    completed string_to_num question

--- small_problems/string_to_num.rb
+++ small_problems/string_to_num.rb
@@ -1,16 +1,19 @@
 DIGITS = {"0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4,
             "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9}
 
-def string_to_integer(array)
-  char_array = array.join("")
-  
+def string_to_integer(str)
+  char_array = str.split("")
+  store = char_array.map do |char|
+    DIGITS[char]
+  end
+  store.inject {|a,i| a*10 + i}
 end
 
 
 
-#
-# p string_to_integer('4321')
-# p string_to_integer('570')
+
+p string_to_integer('4321')
+p string_to_integer('570')
 
 
 
