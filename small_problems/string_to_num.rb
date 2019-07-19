6d9d460a93cdeabb96dea94364ee38c399b3ec81
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-19 18:31:33 +0200

    using a hash for digit conversion, stuck on method

---
+++ small_problems/string_to_num.rb
@@ -0,0 +1,20 @@
+DIGITS = {"0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4,
+            "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9}
+
+def string_to_integer(array)
+  char_array = array.join("")
+  
+end
+
+
+
+#
+# p string_to_integer('4321')
+# p string_to_integer('570')
+
+
+
+#   if str_array.include?(num.to_s)
+#     integer_output << num
+#   end
+# end
