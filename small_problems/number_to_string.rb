0fad1eaa1a52b20ddef05945ef333d5c60f8fa1d
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-21 17:08:40 +0200

    wrote some pseudo code for string to num

---
+++ small_problems/number_to_string.rb
@@ -0,0 +1,15 @@
+DIGITS = {0 => "0", 1 => "1", 2 => "2", 3 => "3", 4 => "4",
+            5 => "5", 6 => "6", 7 => "7", 8 => "8", 9 => "9"}
+
+
+def integer_to_string(number)
+
+end
+
+
+
+# pseudocode
+
+# convert number to array with those numbers
+# use map method and turn each number into corresponding string using hash
+# output all those strings joined together using join method.
