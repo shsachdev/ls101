25636d031c0fa3395d3855930e482749da11ac89
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-23 16:08:25 +0200

    solved integer sign string

--- small_problems/integer_sign_string.rb
+++ small_problems/integer_sign_string.rb
@@ -1,6 +1,13 @@
+require_relative "number_to_string.rb"
+
 def signed_integer_string(number)
-  result_array = []
-  if number == 0
-    result_array = [0]
-  else
+  case number <=> 0
+  when -1 then "-#{integer_to_string(-number)}"
+  when +1 then "+#{integer_to_string(number)}"
+  else          integer_to_string(number)
+  end
 end
+
+p signed_integer_string(4321) == '+4321'
+p signed_integer_string(-123) == '-123'
+p signed_integer_string(0) == '0'
--- small_problems/number_to_string.rb
+++ small_problems/number_to_string.rb
@@ -18,9 +18,7 @@ def integer_to_string(number)
   store.join("")
 end
 
-p integer_to_string(4321) == '4321'
-p integer_to_string(0) == "0"
-p integer_to_string(5000) == '5000'
+
 
 # pseudocode
 
