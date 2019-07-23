ec5f489a4c80d966a98c168d405a58401b33743e
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-23 23:07:12 +0200

    stuck on implementing the hours part

--- small_problems/midnight_pt1.rb
+++ small_problems/midnight_pt1.rb
@@ -1,9 +1,14 @@
 def time_of_day(num)
+  if num == 0
+    return "00:00"
+  end
   minutes = num % 60
   if minutes <= 9
-    minutes = 16
+    minutes = minutes.digits.unshift(0).join("")
+  else
+    minutes = minutes.to_s
   end
-  puts "hello"
+
 end
 
 
@@ -13,5 +18,9 @@ end
 # output will have a ":" in the middle.
 # format of output is: hh:mm
 # to get the minutes, you do input % 60. so that handles the "second" part of the string output
-  # however, note that if minute is a single digit, you need to append a zero to its beginning.
-# now, to get the first part,
+  # however, note that if minute is a single digit, you need to append a zero to its beginning.done.
+  # minutes is now ready to go as a string.
+# now, to get the first part (__:).
+  # some thoughts:
+  # if num is > 0 and < 60, then hh == 00
+  # maybe try some kind of hash?
