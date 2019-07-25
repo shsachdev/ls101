ce79a0086b3b19dc037d219bb0b9da0cfb3c22eb
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-25 15:19:10 +0200

    very close, just need to find a way to check for first element

--- small_problems/cleanup.rb
+++ small_problems/cleanup.rb
@@ -3,14 +3,16 @@ NON_ALPHABET = ["+", "*", "&", "?", "!", "-", "'", "-" ]
 def cleanup(string)
   array = string.split("")
   store = array.map do |char|
+
     if NON_ALPHABET.include?(char) && NON_ALPHABET.include?(array[array.find_index(char) - 1]) != true
       " "
     elsif NON_ALPHABET.include?(char) && NON_ALPHABET.include?(array[array.find_index(char) - 1]) == true
-      nil
+      1
     else
       char
     end
   end
+  store.delete(1)
   store.join()
 end
 
@@ -20,3 +22,4 @@ p cleanup("---what's my +*& line?")
 # turn string into array that is comprised of each character in the string. using the split method.
 # iterate through the array. if the character is not a letter, replace it with a space.
   # but if multiple non-alphabet chars occur in a row, you should only have one space in the result.
+  # if previous element is alpha-numeric, then
