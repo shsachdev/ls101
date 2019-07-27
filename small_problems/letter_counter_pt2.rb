0f016b8de8c29f24fed01c8c37eb9e2ebfab30b2
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-27 17:12:55 +0200

    implement clean_chars but still getting error

--- small_problems/letter_counter_pt2.rb
+++ small_problems/letter_counter_pt2.rb
@@ -1,5 +1,16 @@
+
+ALPHABET = ("a".."z").to_a
+
 def word_sizes(string)
-  word_size = string.split(" ").map do |word|
+  clean_chars = []
+  string.chars.each do |char|
+    if char == " "
+      clean_chars << char
+    elsif ALPHABET.include?(char)
+      clean_chars << char
+    end
+  end
+  word_size = clean_chars.join.split(" ").map do |word|
     word.size
   end
   unique_size = word_size.uniq
@@ -7,5 +18,11 @@ def word_sizes(string)
   unique_size.each do |elem|
     hash[elem] = word_size.count(elem)
   end
-  hash
+  p hash
 end
+
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+# p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+# p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+# p word_sizes('') == {}
