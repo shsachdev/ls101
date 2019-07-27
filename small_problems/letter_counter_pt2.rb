f37f03d151ea9332fb10d669bbd4734381fb1c30
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-27 17:16:44 +0200

    made it work, needed to invoke downcase on each char

--- small_problems/letter_counter_pt2.rb
+++ small_problems/letter_counter_pt2.rb
@@ -4,9 +4,9 @@ ALPHABET = ("a".."z").to_a
 def word_sizes(string)
   clean_chars = []
   string.chars.each do |char|
-    if char == " "
+    if char.downcase == " "
       clean_chars << char
-    elsif ALPHABET.include?(char)
+    elsif ALPHABET.include?(char.downcase)
       clean_chars << char
     end
   end
@@ -18,11 +18,11 @@ def word_sizes(string)
   unique_size.each do |elem|
     hash[elem] = word_size.count(elem)
   end
-  p hash
+  hash
 end
 
 
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
-# p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
-# p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
-# p word_sizes('') == {}
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+p word_sizes('') == {}
