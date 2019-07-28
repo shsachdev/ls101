fd60cb0838fdf520a085081bde0343f6f3aa344b
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-28 23:14:38 +0200

    done

--- small_problems/bannerizer.rb
+++ small_problems/bannerizer.rb
@@ -1,8 +1,18 @@
-def print_in_box(string)
 
+def print_in_box(message)
+   horizontal_rule = "+#{'-' * (message.size + 2)}+"
+   empty_line = "|#{' ' * (message.size + 2)}|"
+
+   puts horizontal_rule
+   puts empty_line
+   puts "| #{message} |"
+   puts empty_line
+   puts horizontal_rule
 end
 
 
 
+
+
 # input = string, output = string
-# 
+#
