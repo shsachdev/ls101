149622acdeed923ec005aa575d567b98e5b62a89
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-28 22:47:23 +0200

    almost, but need to convert back to number format

--- small_problems/alphabet_numbers.rb
+++ small_problems/alphabet_numbers.rb
@@ -1,7 +1,27 @@
-def alphabetic_number_sort(array)
 
+DICTIONARY = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven+", "twelve", "thirteen", "fourteen",
+              "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+
+
+def alphabetic_number_sort(array)
+  word_array = []
+  array.each do |num|
+    word_array << DICTIONARY[num]
+  end
+  p word_array.sort
 end
 
 
 # input = array, output = array
-# turn the numbers into their equivalent words, and then sort those words. 
+# turn the numbers into their equivalent words
+  # you could have a hash with each number as a key and their corresponding word as a value. But is there a faster way to do this?
+    # Instead of hash, we could use an array.
+# then order those words using the sort method.
+
+
+
+
+p alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
