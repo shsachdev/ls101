d3bbdbadf86928bcae560be739cfa1f501267b3e
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-29 16:43:37 +0200

    1 line solution

--- small_problems/delete_vowels.rb
+++ small_problems/delete_vowels.rb
@@ -6,14 +6,18 @@ def remove_vowels(array)
   array.join(" ").chars.each do |letter|
     non_vowel << letter unless VOWELS.include?(letter.downcase)
   end
-  non_vowel.join.split
+  p non_vowel
+  p non_vowel.join
+  p non_vowel.join.split
 end
 
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
-p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ'] #=> false
 
 
-# input = array of strings, output = array of strings (with vowels removed)
-# first, create an array that lists all vowels => ["a", "e", "i", "o", "u"]
-# second,
+# solution shows much easier way to solve problem.
+
+def remove_vowels(string)
+  strings.map {|string| string.delete("aeiouAEIOU")}
+end
