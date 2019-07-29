58d63969f5da408eac59c8d0bdb1b19c1d5ca241
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-29 16:31:10 +0200

    need to register empty string

--- small_problems/delete_vowels.rb
+++ small_problems/delete_vowels.rb
@@ -3,21 +3,17 @@ VOWELS = ["a", "e", "i", "o", "u"]
 
 def remove_vowels(array)
   non_vowel = []
-  array.each do |word|
-    word.split("").each do |letter|
-      non_vowel << letter unless VOWELS.include?(letter.downcase)
-    end
+  array.join(" ").chars.each do |letter|
+    non_vowel << letter unless VOWELS.include?(letter.downcase)
   end
-  p non_vowel
+  non_vowel.join.split
 end
 
-remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
-remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
-remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
 
 
 # input = array of strings, output = array of strings (with vowels removed)
 # first, create an array that lists all vowels => ["a", "e", "i", "o", "u"]
-# iterate through each word in the input array, and then iterate once more through each letter
-# if the letter is included in the vowels array, then remove that letter. else, keep the letter.
-#
+# second,
