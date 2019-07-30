0d7d772c93855d665e80f3f3c0b7824e247b1515
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-30 16:45:34 +0200

    cleaned up folders

---
+++ small_problems/easy_problems/reverse_array_pt1.rb
@@ -0,0 +1,24 @@
+def reverse!(array)
+  
+end
+
+list = [1,2,3,4]
+result = reverse!(list)
+result == [4, 3, 2, 1]
+list == [4, 3, 2, 1]
+list.object_id == result.object_id
+
+list = %w(a b e d c)
+reverse!(list) == ["c", "d", "e", "b", "a"]
+list == ["c", "d", "e", "b", "a"]
+
+list = ['abc']
+reverse!(list) == ["abc"]
+list == ["abc"]
+
+list = []
+reverse!(list) == []
+list == []
+
+# input = array, output = array, but reversed; both arrays are the same object.
+#
