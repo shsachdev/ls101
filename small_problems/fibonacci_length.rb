6d8844317b4ac1243233b6196ccdcaec5c5d6a66
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-30 16:42:28 +0200

    fibonnaci_length solved

--- small_problems/fibonacci_length.rb
+++ small_problems/fibonacci_length.rb
@@ -1,26 +1,28 @@
 def find_fibonacci_index_by_length(num)
-  i = 1
+  i = 2
   fib_num_1 = 1
   fib_num_2 = 1
   loop do
     fib_num_1 = fib_num_2 + fib_num_1
+    i += 1
+    break if fib_num_1.digits.size == num
     fib_num_2 = fib_num_2 + fib_num_1
     i += 1
+    p i
     break if fib_num_2.digits.size == num
   end
   i
 end
 
 
-p find_fibonacci_index_by_length(2)
 
 
-# find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
-# find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
-# find_fibonacci_index_by_length(10) == 45
-# find_fibonacci_index_by_length(100) == 476
-# find_fibonacci_index_by_length(1000) == 4782
-# find_fibonacci_index_by_length(10000) == 47847
+# p find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+# p find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+# p find_fibonacci_index_by_length(10) == 45
+# p find_fibonacci_index_by_length(100) == 476
+# p find_fibonacci_index_by_length(1000) == 4782
+p find_fibonacci_index_by_length(10000) == 47847
 
 
 
