0c667473ba16a471dfdaaf42fe96db8cfc72ea23
Shikhar Sachdev <shsachdev@gmail.com>
2019-07-31 09:58:28 +0200

    solved halvsies using slice method

--- small_problems/easy_problems/halvsies.rb
+++ small_problems/easy_problems/halvsies.rb
@@ -1,2 +1,17 @@
 def halvsies(array)
+  nested_array = []
+  if array.size.even?
+    nested_array[0] = array.slice(0..(array.size/2)-1)
+    nested_array[1] = array.slice((array.size/2)..-1)
+  else
+    nested_array[0] = array.slice(0..(array.size/2))
+    nested_array[1] = array.slice((array.size/2)+1..-1)
+  end
+  nested_array
 end
+
+
+p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+p halvsies([5]) == [[5], []]
+p halvsies([]) == [[], []]
