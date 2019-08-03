5d202265b8f9e72c0dcca0d51341bf881a723ba1
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-02 18:10:05 -0700

    solved middle_char

---
+++ small_problems/easy_problems/middle_character.rb
@@ -0,0 +1,15 @@
+def center_of(str)
+  if str.size.odd?
+    return str[str.size / 2]
+  else
+    return str[(str.size / 2) - 1] + str[str.size / 2]
+  end
+end
+
+p center_of('I love ruby') == 'e'
+p center_of('Launch School') == ' '
+p center_of('Launch') == 'un'
+p center_of('Launchschool') == 'hs'
+p center_of('x') == 'x'
+
+# input = str, output = char of that str (so another str)
