97075f3856c73e8c417d09f0637c743a06d09a11
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-02 18:46:26 -0700

    solved double_doubles

---
+++ small_problems/easy_problems/double_doubles.rb
@@ -0,0 +1,33 @@
+def is_double_number?(num)
+  array = num.to_s.chars
+  if array.size.odd?
+    return false
+  else
+    if array.slice(0..array.size/2-1) == array.slice(array.size/2..-1)
+      return true
+    else
+      return false
+    end
+  end
+end
+
+def twice(num)
+  if is_double_number?(num)
+    return num
+  else
+    return num * 2
+  end
+end
+
+p twice(37) == 74
+p twice(44) == 44
+p twice(334433) == 668866
+p twice(444) == 888
+p twice(107) == 214
+p twice(103103) == 103103
+p twice(3333) == 3333
+p twice(7676) == 7676
+p twice(123_456_789_123_456_789) == 123_456_789_123_456_789
+p twice(5) == 10
+
+# input = number, output = number
