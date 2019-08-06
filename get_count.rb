9295b8e1992deae01f1158a7d4a6fe75d42e8c33
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-05 20:34:29 -0700

    practice for assessment

---
+++ get_count.rb
@@ -0,0 +1,13 @@
+VOWELS = ["a", "e", "i", "o", "u"]
+
+def get_count(str)
+  str.chars
+end
+
+p get_count("abracadabra")
+
+# p get_count("abracadabra") == 5
+
+# return the number of vowels in a string
+
+# turn str into an array of characters.
