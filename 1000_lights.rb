ae7bc5b332e39379834f281f813160e976a17345
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-06 23:34:57 -0700

    it works yayyyyy

--- 1000_lights.rb
+++ 1000_lights.rb
@@ -8,20 +8,25 @@ def lights_on(total)
   end
   counter1 = 2
   counter2 = 0
-  while counter1 < total
+  while counter1 < total + 1
     loop do
       light_toggle[counter2] += 1 if (counter2 + 1)%counter1 == 0
+      light_toggle
       break if counter2 == light_toggle.size - 1
       counter2 = counter2 + 1
-      p light_toggle
     end
     counter2 = 0
     counter1 += 1
   end
-  light_toggle
+  arr = []
+  light_toggle.each_with_index do |elem, index|
+    arr << index + 1 if elem.odd?
+  end
+  arr
 end
 
-lights_on(10)
+p lights_on(5)
+p lights_on(10)
 
 # we will let 0 represent that the light is off, and 1 represent that the light is on
 
@@ -31,20 +36,3 @@ lights_on(10)
   # if hash_key.index is divisible by the divisor, then toggle. if not, don't toggle.
   # then augment the divisor by 1 (keep doing this until we have reached total)
 # then return the number of 1s in the array.
-
-
-#   counter = 2
-#   loop do
-#     light_toggle.each_with_index do |switch, index|
-#       if index%counter == 0
-#         light_toggle[switch] += 1
-#         p light_toggle
-#       end
-#     end
-#     counter += 1
-#     break if counter = total + 1
-#   end
-#   light_toggle
-# end
-# #
-# lights_on(5)
