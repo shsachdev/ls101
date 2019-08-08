b312c064977415db84cd1ae4b2d604b175d01e91
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-08 05:52:54 -0700

    just some warmup

---
+++ list_include_again.rb
@@ -0,0 +1,18 @@
+def include?(arr, value)
+  same_numbers = []
+  arr.each do |num|
+    same_numbers << num if (num <=> value) == 0
+  end
+  if same_numbers.size > 0
+    true
+  else
+    false
+  end
+end
+
+
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
