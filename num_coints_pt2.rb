1aee0b9eba1dd06f2a01b5d455fce5bcc19ae36b
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-11 16:40:55 -0700

    solved second google challenge

--- num_coints_pt2.rb
+++ num_coints_pt2.rb
@@ -1,17 +1,33 @@
+def unique_combinations(array) # => returns all different combinations of coin values.
+  array.permutation.to_a
+end
+
+# unique_combinations([25, 10, 1]) => [[25,10,1], [10,25,1], [10,1,25], [25,1,10],[1,10,25], [1,25,10]]
+
+def calculator(array, total)
+  sum = 0
+  array.each do |num|
+    sum += total / num
+    total = total - ((total / num)*num)
+  end
+  sum
+end
+
 def num_coins(cents)
   coin_values = [25,10,1]
+  diff_coin_values = unique_combinations(coin_values)
   num_coins = []
-  coin_values.each do |value|
-    num_coins << cents / value
-    cents = cents - ((cents / value)*value)
+  diff_coin_values.each do |sub_array|
+    num_coins << calculator(sub_array, cents)
   end
-  num_coins.sum
+  num_coins.min
 end
 
 
-p num_coins(33) == 5
-p num_coins(100) == 4
-p num_coins(31) == 3
+
+# p num_coins(33) == 5
+# p num_coins(100) == 4
+p num_coins(31) == 4
 
 # 33: 1 quarter (25), 1 nickel (5), 3 pennies (1), [dime 10]
 
@@ -21,4 +37,7 @@ p num_coins(31) == 3
 
 # Algorithm
 
-# 1.
+# 1. get all the unique ways of structuring coin_values (unique combinations.)
+  # define new method called unique_comb.
+  # input = arr of elements, output = array of sub_arrays
+# 2. iterate through all of them, pick the one that returns the lowest number of coins.
