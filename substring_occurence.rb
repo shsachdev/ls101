0edcb81e04fd0b8718acf5dfbd402f058e1ffa6a
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-13 23:05:34 -0700

    code works

--- substring_occurence.rb
+++ substring_occurence.rb
@@ -1,4 +1,45 @@
+def downcase(array)
+  array.map do |char|
+    char.downcase
+  end
+end
+
+
+def find_substrings(string)
+  letters = string.chars
+  substrings = []
+  start_tracker = 0
+  loop do
+    end_tracker = letters.size - 1
+    loop do
+      break if start_tracker == end_tracker
+      substrings << letters[start_tracker..end_tracker]
+      end_tracker = end_tracker - 1
+    end
+    start_tracker = start_tracker + 1
+    break if start_tracker == letters.size - 1
+  end
+  substrings.map do |sub_arr|
+    downcase(sub_arr)
+  end
+end
+
+
 def substring_test(str1, str2)
+  if str1 == "" || str2 == ""
+    return false
+  end
+  str1_substrings = find_substrings(str1)
+  str2_substrings = find_substrings(str2)
+  duplicates = []
+  str1_substrings.each do |sub_arr|
+    duplicates << sub_arr if str2_substrings.include?(sub_arr)
+  end
+  if duplicates.size == 0
+    return false
+  else
+    return true
+  end
 end
 
 # Given 2 strings, your job is to find out if there is a substring
@@ -26,10 +67,15 @@ p substring_test('supercalifragilisticexpialidocious', 'SoundOfItIsAtrociou') ==
 # Algorithm
 
 # 1. find all substrings of 1st string. store in array.
+  # define a new method substring.
+  # two "trackers": one to keep track of start, the other to keep track of end.
+  # tracker_1 starts at S. tracker_2 starts at g. tracker_2 works its way to o.
+    # once we have all those substrings. tracker_1 starts at o. etc
+  # datastructures: get chars of string, store in array.
 
 # 2. find all substrings of 2nd string. store in array.
 
-# 3. compare the two arrays; if overlap, store in duplicates (array).
+# 3. compare the two arrays; if overlap, store in duplicates (array). (and check that substring size is greater than 1)
   # iterate through either array (say 1st one), and see if it includes substring in other array.
 
 #4. if duplicates.size > 0, return true.
