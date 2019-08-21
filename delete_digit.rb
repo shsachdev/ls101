42241bd4c8c5c6567ae49037c292f67dc9c18822
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-20 19:56:14 -0700

    i am not sure with this problem

--- delete_digit.rb
+++ delete_digit.rb
@@ -1,36 +1,35 @@
 # Given an integer num, find the maximal number you can obtain by deleting exactly
 # one digit of the given number.
 
-
-def get_two_digits(arr)
+def array_arrangements(arr)
+  multiple_holder = []
   first_index = 0
-  element_combinations = []
   loop do
-    break if first_index == arr.size - 1
-    second_index = first_index + 1
-    loop do
-      element_combinations << arr[first_index].to_s + arr[second_index].to_s
-      second_index = second_index + 1
-      break if second_index == arr.size
+    temp_holder = []
+    arr.each_with_index do |num, index|
+      temp_holder << num unless index == first_index
     end
+    multiple_holder << temp_holder.permutation.to_a
+    break if first_index = arr.size + 1
     first_index = first_index + 1
   end
-  p element_combinations
+  p multiple_holder
 end
 
+array_arrangements([1, 5, 2])
 
-def delete_digit(num)
-  digits_num = num.digits.reverse
-  two_digit_nums = []
-  get_two_digits(digits_num).each do |str|
-    two_digit_nums << str.to_i && two_digit_nums << str.reverse.to_i
-  end
-  two_digit_nums.max
-end
-
-p delete_digit(152) == 52
-
-p delete_digit(1001) == 101
+# def delete_digit(num)
+#   digits_num = num.digits.reverse
+#   two_digit_nums = []
+#   get_two_digits(digits_num).each do |str|
+#     two_digit_nums << str.to_i && two_digit_nums << str.reverse.to_i
+#   end
+#   two_digit_nums.max
+# end
+#
+# p delete_digit(152) == 52
+#
+# p delete_digit(1001) == 101
 
 
 # pseudocode
@@ -41,11 +40,10 @@ p delete_digit(1001) == 101
 
 # Algorithm
 
-# 1. store the digits of num in an array called digits_num
+# 1. store the digits of num in an array called digits_num. assume the array has n elements.
 
-# 2. then find a way to get all 2 element combinations of that array.
-  # [1,5,2] => [[1,5], [1,2], [5,2]]
-  # create 2 loops. one keeping track of first index, one keep track of second index.
+# 2. find a way to get all sub_arrays with n-1 element arrangements.
+
 
 # 3. then invoke a method one each of those 2 element arrays that returns the 2 integers formed from each array, and appends it
 # to a new array called two_digit_nums.
