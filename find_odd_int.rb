8c92b9bc1debaf6359fadd2d26334d32bf597319
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-20 13:24:57 -0800

    doing some alg problems for refresher

---
+++ find_odd_int.rb
@@ -0,0 +1,8 @@
+# Given an array, find the integer that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+
+def find_int(seq)
+end
+
+# input = array, output = integer
--- min_sum_v2.rb
+++ min_sum_v2.rb
@@ -2,7 +2,8 @@
 # summing each Two integers product.
 
 def min_sum(array)
-
+  array.sort!
+  (0...array.size/2).map {|i| arr[i] * arr[-i-1]}.sum
 end
 
 puts min_sum([5,4,2,3]) == 22
@@ -16,4 +17,7 @@ puts min_sum([9,2,8,7,5,4,0,6]) == 74
 
 # 1. Find each two integer product.
 # 2. Then, find the min sum of all those products where each two integer product
-  # is of 2 numbers that haven't been utilized before. 
+  # is of 2 numbers that haven't been utilized before.
+# 3. Return that min sum.
+
+# Is there a way to do this without keeping track of which numbers have been used for each product?
