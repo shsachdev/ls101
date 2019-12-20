462ddb5b07499a4a6aadeb256cf9873e9b5aa6a4
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-20 13:29:13 -0800

    find odd int problem completed

--- find_odd_int.rb
+++ find_odd_int.rb
@@ -3,6 +3,14 @@
 # There will always be only one integer that appears an odd number of times.
 
 def find_int(seq)
+  arr = seq.select {|num| seq.count(num).odd?}
+  arr[0]
 end
+
+puts find_int([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) == 5
+puts find_int([1,1,2,-2,5,2,4,4,-1,-2,5]) == -1
+puts find_int([20,1,1,2,2,3,3,5,5,4,20,4,5]) == 5
+puts find_int([10]) == 10
+
 
 # input = array, output = integer
