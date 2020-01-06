678f13545045dbdbabaaff3dc8412334370ff7a6
Shikhar Sachdev <shsachdev@gmail.com>
2020-01-07 00:08:09 +0100

    working on egyptian_fractions

---
+++ egyptian_fractions.rb
@@ -0,0 +1,14 @@
+# Rational number == any number that can represented as the result of the divison
+# between two integers (1/3, 22/7)
+
+# Unit Fraction == a rational number where the numerator is 1.
+
+# egyptian fraction == sum of a series of distinct unit fractions.
+
+# Write two methods: one that takes a Rational number as an argument, and returns
+# an Array of the denominators that are part of an Egyptian Fraction representation
+# of the number, and another that takes an Array of numbers in the same format,
+# and calculates the resulting Rational number. You will need to use the Rational class
+# provided by Ruby.
+
+puts Rational(1)
