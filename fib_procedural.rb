4ac261afff4a98ef6261340f264784cf6356d21d
Shikhar Sachdev <shsachdev@gmail.com>
2020-01-29 09:10:41 -0800

    added some exercises

---
+++ fib_procedural.rb
@@ -0,0 +1,12 @@
+def fibonacci(nth)
+  first, last = [1, 1]
+  3.upto(nth) do
+    first, last = [last, first + last]
+  end
+
+  last
+end
+
+puts fibonacci(20) == 6765
+
+puts fibonacci(100) == 354224848179261915075
